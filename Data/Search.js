cd8d7e0c74a10d5307c316407f117deebc53ea87
sw-intelligent-mapping <sw-intelligent-mapping@ge.com>
2016-08-17 09:05:10 +0100

    Updated due to performance page

--- Data/Search.js
+++ Data/Search.js
@@ -1,1 +1,1 @@
-define({r:0.5,st:'Stem',n:1,t:[0],u:['../Content/IntelligentMappingServices/AddingServicesToApplication.htm'],s:['1'],p:[['1',1],['to',1]]});
+define({r:0.5,st:'Stem',n:1,t:[0],u:['../Content/IntelligentMappingServices/AddingServicesToApplication.htm'],s:['0'],p:[['0',8],['secret',6]]});
