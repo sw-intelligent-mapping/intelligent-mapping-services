7788ada0838c31febe50419aa6679cdf7958424f
MickyRunham <michael.runham@ge.com>
2018-01-18 14:59:58 +0000

    Add files via upload

--- Data/Search.js
+++ Data/Search.js
@@ -1,1 +1,1 @@
-define({r:0.5,st:'Stem',n:1,t:[0],u:['../Content/A_Navigation/Copyright/swCopyright.htm'],s:['$','xmlhttprequest'],p:[['$',12],['documentation',17],['name',13],['there',11]]});
+define({r:0.5,st:'Stem',n:1,t:[0],u:['../Content/A_Navigation/Copyright/swCopyright.htm'],s:['$'],p:[['$',12],['dynamic',3],['non',11],['tiered',4]]});
